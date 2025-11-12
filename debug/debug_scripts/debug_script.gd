338297c5e279f2ff75f472579110e56a6b0cd768
DulceGir <avatargir@gmail.com>
2025-11-11 18:53:15 -0800

    Add files via upload
    This commit should hopefully give a system of file structure, aka, tell people where to plug their files in to be used in development of the game. I've also included README files for the folders for the sake of guidance of structure.
    
    All of this is entirely open to advice / recommendations / requests / etc.

--- debug/debug_scripts/debug_script.gd
+++ debug/debug_scripts/debug_script.gd
@@ -1,5 +1,9 @@
 extends Node3D
 
-# Rotate the cube
+# Move the cube
 func _process(delta: float) -> void:
-	self.rotation.y += (PI/6)*delta
+	if(Input.is_action_pressed("move_up")) : position.y += .2
+	if(Input.is_action_pressed("move_down")) : position.y -= .2
+	if(Input.is_action_pressed("move_right")) : position.x += .2
+	if(Input.is_action_pressed("move_left")) : position.x -= .2
+	
---
+++ scripts/README_Scripts.txt
@@ -0,0 +1,3 @@
+The purpose of the scripts folder is to store all the code.
+
+The game will be written in GDScript, unless we would wish to switch to C# for a specific reason.
